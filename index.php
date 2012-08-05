682ba461b17008c944a9e85f92c447b88736c2b3
YanShen <jengyan@gmail.com>
2012-08-05 18:31:22 +0800

    redirect index.php to latest_survey.php

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
-<?php
-
-include('classes/main.class.php');
-include('classes/survey.class.php');
-
-$survey = new UCCASS_Survey;
-
-echo $survey->com_user_header();
-
-echo $survey->latest_surveys2();
-
-echo $survey->com_footer();
-//echo $survey->com_footer();
-
-?>
+<html>
+	<script>
+	document.location="/ms/latest_surveys.php";	
+	</script>
+</html>
