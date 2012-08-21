d3994eb57df8eec0e63c29736afd46eadc72b87b
YanShen <jengyan@gmail.com>
2012-08-22 00:02:20 +0800

    fix issue: No pop up after registering personal info to a survey.

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <html>
 	<script>
-	document.location="/ms/latest_surveys.php";	
+	function getUrlVars()
+	{
+		var vars = [], hash;
+		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+		for(var i = 0; i < hashes.length; i++)
+		{
+			hash = hashes[i].split('=');
+			vars.push(hash[0]);
+			vars[hash[0]] = hash[1];
+		}
+		return vars;
+	}
+	
+	var result = getUrlVars()["result"];
+	document.location="/ms/latest_surveys.php" + (result ? "?result="+result : "");
 	</script>
 </html>
